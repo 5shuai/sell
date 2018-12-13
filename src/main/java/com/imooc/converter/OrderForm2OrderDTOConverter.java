f48c3cbc46ef5d5f2a5b6b6cb254c843a49fdc2e
5shuai <763432695@qq.com>
2018-12-13 17:23:50 +0800

    增加pom.xml

---
+++ src/main/java/com/imooc/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,8 @@
+package com.imooc.converter;
+
+/**
+ * @author wuss.
+ * @date 2018/12/13 12:58
+ */
+public class OrderForm2OrderDTOConverter {
+}
---
+++ src/main/java/com/imooc/utils/serializer/Date2LongSerializer.java
@@ -0,0 +1,8 @@
+package com.imooc.utils.serializer;
+
+/**
+ * @author wuss.
+ * @date 2018/12/13 16:26
+ */
+public class Date2LongSerializer {
+}
