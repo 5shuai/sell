b78a32d0e201dd93a29f86adc10ea8354f3a10a9
5shuai <763432695@qq.com>
2019-12-11 15:35:07 +0800

    add codes

--- src/main/java/com/imooc/SellApplication.java
+++ src/main/java/com/imooc/SellApplication.java
@@ -2,10 +2,15 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SellApplication {
-
+public class SellApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SellApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
     }
