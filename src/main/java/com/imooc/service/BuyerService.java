4d3d3cbaab5bfffd56bbbbb4616db83c4bba9277
5shuai <763432695@qq.com>
2019-12-11 17:52:05 +0800

    add codes

--- src/main/java/com/imooc/service/BuyerService.java
+++ src/main/java/com/imooc/service/BuyerService.java
@@ -7,7 +7,7 @@ import com.imooc.dto.OrderDTO;
  * @date 2018/12/13 20:03
  */
 public interface BuyerService {
-    //查询一个订单111
+    //查询一个订单
     OrderDTO findOrderOne(String userId,String orderId);
     //取消订单
     OrderDTO cancelOrder(String userId, String orderId);
